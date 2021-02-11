2c27725ec44d3f6c3d53afc268a2e3661628d6d2
redy <redy.aries12@gmail.com>
2021-02-11 11:17:16 -0500

    game refactored

--- app.js
+++ app.js
@@ -1,85 +1,134 @@
-// define the selection of the computer
+// initialize
+let playerImageContainer = document.querySelector('.player-image');
+let computerImageContainer = document.querySelector('.computer-image');
+let playerScore = 0;
+let computerScore = 0;
+let player = document.querySelector('.player-score');
+let computer = document.querySelector('.computer-score');
+let body = document.querySelector('body');
+let playerImage = document.querySelector('.player-image > img');
+let computerImage = document.querySelector('.computer-image > img');
+let startGame = document.querySelector('.welcome button');
+let buttons = document.querySelectorAll('.btn');
 
+// start game
+startGame.addEventListener('click', (e) => {
+    e.target.parentNode.style.display = "none";
+    buttons.forEach((button) => {
+        button.classList.add('btn-animation');
+    })
+})
+
+// show the initial score
+showScore(playerScore, computerScore);
+
+// add event listeners to the buttons
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+}) 
+
+
+// define the selection of the computer
 function computerPlay() {
     let option = ["rock", "paper", "scissors"];
     return option[Math.floor(Math.random()*3)];
 }
 
-// define the rules for the game and the posible results
-// 0, 1, 2 are beeing added to the returned messages to identify the winner
-// 0 for draw, 1 if the player wins, 2 if the computer wins
+// define the rules for the game
 
-function playGame(playerSelection, computerSelection) {
+function playRound(e) {
+
+    // get the user selection and show the image
+    let playerSelection = e.target.textContent;
+    // get the computer selection and show the image
+    let computerSelection = computerPlay();
+
+    showImage(playerSelection, computerSelection);
+    let winner;
     if (playerSelection == "rock") {
         if (computerSelection == "rock") {
-            return "0Draw";
+            winner = "draw";
         } else if(computerSelection == "paper") {
-            return "2You lose! papers beats rock.";
+            winner = "computer";
         } else {
-            return "1you win! rock beats scissors";
+            winner = "player";
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return "1You win! paper beats rock.";
+            winner = "player";
         } else if (computerSelection == "paper") {
-            return "0Draw!";
+            winner = "draw";
         } else {
-            return "2you lose! scissors beats paper";
+            winner = "computer";
         }
     } else if (playerSelection == "scissors"){
         if (computerSelection == "rock") {
-            return "2you lose! rock beats scissors";
+            winner = "computer";
         } else if (computerSelection == "paper") {
-            return "1You win! scissors beatas paper";
+            winner = "player";
         } else {
-            return "0Draw!";
+            winner = "draw";
         }
     } else {
         alert("Selection not recognized!!!");
     }
+
+    if (winner == "player") {
+        playerScore += 1;
+    } else if (winner == "computer") {
+        computerScore += 1;
+    }
+        
+    // update the points on the screen
+    showScore(playerScore, computerScore);
+    
+    if (playerScore == 5 || computerScore == 5) {
+        let h1 = document.querySelector('h1');
+        let winner = (playerScore == 5) ? "player" : "computer";
+        let finalResult = document.createElement('div');
+        finalResult.classList.add('winner');
+        let restartButton = document.createElement('button');
+        restartButton.classList.add('restart');
+        restartButton.textContent = "restart"
+        finalResult.appendChild(document.createTextNode(`${winner.toUpperCase()} WINS!!`));
+        finalResult.appendChild(restartButton);
+        restartButton.addEventListener('click', restartGame);
+        body.insertBefore(finalResult,h1);
+    }
 }
 
-// exceute the game 5 times and save the score to find a winner
+// show the score
+function showScore(playerScore, computerScore) {
+    player.textContent = `player score: ${playerScore}`;
+    computer.textContent = `computer score: ${computerScore}`;
+}
 
-function game() {
-    let gameFinalResume = document.getElementById('gameResume');
-    let gameResume = '';
-    let playerSelections = [];
-    let computerSelections = [];
-    let i = 1;
-    let playerScore = 0;
-    let computerScore = 0;
-    let winner;
-    while (i <= 5) {
-        playerSelection = prompt("choose between ROCK, PAPER or SCISSORS: ").toLowerCase();
-        playerSelections.push(playerSelection);
-        computerSelection = computerPlay();
-        computerSelections.push(computerSelection);
-        let result = playGame(playerSelection, computerSelection);
-        console.log(`the player selection was: ${playerSelection}
-The computer selection was: ${computerSelection}`);
-        console.log(result.slice(1,));
-        if (parseInt(result) == 1) {
-            playerScore += 1
-        } else if (parseInt(result) == 2) {
-            computerScore += 1
-        }
-        i += 1
-    }
-    if (playerScore > computerScore) {
-        winner = `You win, you have ${playerScore} points and computer has ${computerScore} points.`;
-    } else if(playerScore < computerScore) {
-        winner = `You lose, you have ${playerScore} points and computer has ${computerScore} points.`;
-    } else {
-        winner = `Draw, you have ${playerScore} points and computer has ${computerScore} points.`;
-    }
-    console.log(winner);
-    gameResume = `${winner}<br>your selections are >> ${playerSelections.join(" - ")}<br>the computer selections are >> ${computerSelections.join(" - ")}`
+//show the image
+function showImage(playerSelection, computerSelection) {
+    playerImage.src = `./images/${playerSelection}.png`;
+    computerImage.src = `./images/${computerSelection}.png`;
 
-    gameFinalResume.innerHTML = gameResume;
+    console.log(playerImageContainer.classList);
+    playerImageContainer.classList.add('player-animation');
+
+    playerImageContainer.addEventListener('animationend', (e) => {
+    playerImageContainer.classList.remove('player-animation');
+    console.log(playerImageContainer.classList);
+    })
+
+    computerImageContainer.classList.add('computer-animation');
+    computerImageContainer.addEventListener('animationend', (e) => {
+        computerImageContainer.classList.remove('computer-animation')
+    });
 
 }
 
-game()
-
-// console.log(playGame(playerSelection, computerSelection));
+// restart the game
+function restartGame(e) {
+    playerScore = 0;
+    computerScore = 0;
+    showScore(playerScore, computerScore);
+    body.removeChild(e.target.parentNode);
+    computerImage.src = "";
+    playerImage.src = "";
+}
